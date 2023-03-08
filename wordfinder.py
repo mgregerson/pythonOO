5c2f142cff97c83d6865be9ec7f3cf5730a70ec0
Justin Clark <jclarksummit@gmail.com>
2023-03-07 20:45:53 -0500

    Finished first two parts

--- wordfinder.py
+++ wordfinder.py
@@ -2,20 +2,23 @@ import random
 
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
-
     def __init__(self, file):
         """Defines file and word_list arguments in our Class"""
         self.file = file
         self.word_list = self.get_word_list()
+        print(f"{len(self.word_list)} words read")
 
+    def __repr__(self):
+        return f"{len(self.word_list)} words read"
+
+    # Add comprehension to line 21
     def get_word_list(self):
         """Generates a list of words in the file"""
         word_file = open(self.file)
-        word_list = []
-        for line in word_file:
-            curr_word = line.replace("\n", "")
-            word_list.append(curr_word)
-            
+        word_list = [line.replace("\n", "") for line in word_file]
+        # for line in word_file:
+        #     curr_word = line.replace("\n", "")
+        #     word_list.append(curr_word)
         return word_list
 
     def len_word_list(self):
@@ -29,29 +32,25 @@ class WordFinder:
 class SpecialWordFinder(WordFinder):
     """SpecialWordFinder: Finds words without comments or blank space"""
 
-    def __init__(self, file):
-        """Defines the constructor for SpecialWordFinder"""
-        super().__init__(file)
-        self.word_list = self.get_word_list()
-
     def get_word_list(self):
         """Returns a word list without blank lines or comment lines"""
-        word_file = open(self.file)
-        word_list = []
-        for line in word_file:
+        word_list = super().get_word_list()
+        new_word_list = []
+        for line in word_list:
             if line.strip() == '':
                 pass
             elif line[0] != '#':
                 curr_word = line.replace("\n", "")
-                word_list.append(curr_word)
-        
-        return word_list
+                new_word_list.append(curr_word)
+
+        self.word_list = new_word_list
+        return self.word_list
 
 
 
 
 
-    
+
 
 
 
